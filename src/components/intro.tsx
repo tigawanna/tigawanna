e584307a28b92bdc41e6b626619c49c168b7eb96
tigawanna <denniskinuthiaw@gmail.com>
2023-04-24 07:10:14 +0300

    push new rakkas

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,43 +1,50 @@
 import GitHubImage from '../res/github.jpg'
-
+import TypeAnimation from 'react-type-animation'
 interface introProps {}
 
 export default function Intro({}:introProps){
   return (
-    <div className="h-[90vh] w-full flex flex-col items-center justify-center ">
+    <div className="mt-[60px] flex min-h-screen w-[100%] flex-col bg-slate-700 pl-4">
+      <div className="font-mono text-2xl font-bold text-green-300 md:text-xl ">
+        Hi there, am
+      </div>
+      <div className="mt-[100px] flex flex-col">
 
-     <div className='p-5 h-full w-full flex flex-col 
-     md:flex-row items-center justify-center md:justify-between gap-2 '>
-
-        <div className="w-[90%] flex flex-col min-w-[70%] gap-5">
-          <h2 className=" mt-[5%] text-7xl md:text-9xl  font-bold text-green-400 gradient-text">
-          Hi there, am Dennis Kinuthia
-        </h2>
-        <h3 className='w-[90%] text-slate-300 text-xl md:text-4xl'>
-          Software engineer with a passion for building and learning new things
-        </h3>
-          <h3 className="   text-green-200  md:text-lg m-0 p-0">
-            ❤️ React
-            ❤️ tailwindcss
-            ❤️ react-query
-            ❤️ GraphQL
-          </h3>
+        <div className="font-sarif mt-[5%] text-6xl font-bold text-green-400 md:text-8xl">
+          Dennis Kinuthia
         </div>
-        
 
-        <div className='w-full md:min-w-[10%] flex justify-center'>
-          <img
-          alt="Dennis Kinuthia"
-          src={GitHubImage} 
-          height="350px"
-          width="350px"
-          className='rounded-2xl aspect-square'/>
+        <div className="font-sarif mt-[30px] text-3xl font-bold text-slate-200 md:text-6xl">
+          <TypeAnimation
+            cursor={true}
+            sequence={['African web developer and designer']}
+            wrapper="h1"
+            className="text-green-300"
+          />
         </div>
+
+        <div className="font-sarif mt-[40px] text-base  text-green-200 md:w-[40%] md:text-lg">
+
+          ❤️ React
+          ❤️ tailwindcss
+          ❤️ react-query
+          ❤️ GraphQL
+        </div>
+
+
+
+
 
       </div>
-
-
     </div>
   )
 }
 
+{/* <div className='w-full md:min-w-[10%] flex justify-center'>
+  <img
+    alt="Dennis Kinuthia"
+    src={GitHubImage}
+    height="350px"
+    width="350px"
+    className='rounded-2xl aspect-square' />
+</div> */}
