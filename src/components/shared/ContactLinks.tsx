5771cd7fd846f42f1724cb1f73bd5378bd20e926
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-12-30 20:13:39 +0300

    fix up links

--- src/components/shared/ContactLinks.tsx
+++ src/components/shared/ContactLinks.tsx
@@ -1,25 +1,60 @@
 import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaDev, FaTwitter } from "react-icons/fa";
-
+import { FaGithub, FaLinkedinIn, FaDev, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 interface ContactLinksProps {
   size?: number;
 }
 
 export function ContactLinks({ size = 30 }: ContactLinksProps) {
+  const contacts ={
+     nickname:"tigawanna",
+    phone:"+254790984481",
+    email:"denniskinuthiawaweru@gmail.com"
+  }
   return (
     <div className="p-1  flex  gap-4">
-      <Link href="https://github.com/tigawanna" target="_blank">
+      <Link
+        href={"https://github.com/" + contacts.nickname}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={contacts.nickname}>
         <FaGithub className="w-7 h-7 hover:text-secondary" />
       </Link>
-      <Link href="https://linkedin.com/in/dennis-kinuthia" target="_blank">
+      <Link
+        href="https://linkedin.com/in/dennis-kinuthia"
+        target="_blank"
+        rel="noopener noreferrer"
+        title={"dennis-kinuthia"}>
         {" "}
         <FaLinkedinIn className="w-7 h-7 hover:text-secondary" />
       </Link>
-      <Link href="https://dev.to/tigawanna" target="_blank">
+      <Link
+        href={"https://dev.to/" + contacts.nickname}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={contacts.nickname}>
         <FaDev className="w-7 h-7 hover:text-secondary" />
       </Link>
-      <Link href="https://twitter.com/tigawanna" target="_blank">
+      <Link
+        href={"https://twitter.com/" + contacts.nickname}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={contacts.nickname}>
         <FaTwitter className="w-7 h-7 hover:text-secondary" />
+      </Link>
+      <Link
+        href={"mailto:" + contacts.email}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={contacts.email}>
+        <HiOutlineMail className="w-7 h-7 hover:text-secondary" />
+      </Link>
+      <Link
+        href={"https://wa.me/" + contacts.phone}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={contacts.phone}>
+        <FaWhatsapp className="w-7 h-7 hover:text-secondary" />
       </Link>
     </div>
   );
