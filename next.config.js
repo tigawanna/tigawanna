3ff64dbc7b536f6af25486da9f178fe25c3f6862
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-01-06 14:32:33 +0300

    big update

--- next.config.js
+++ next.config.js
@@ -4,14 +4,31 @@ const nextConfig = {
   //     serverActions: true,
   //   },
   images: {
-    domains: [
-      "127.0.0.1",
-      "tigawanna-pocketbase.fly.dev",
-      "opengraph.githubassets.com",
-      "lh3.googleusercontent.com",
-      "repository-images.githubusercontent.com",
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "https",
+        hostname: "tigawanna-pocketbase.fly.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "opengraph.githubassets.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "repository-images.githubusercontent.com",
+      },
     ],
+
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,18 @@ import { Inter } from "next/font/google";
 import { siteConfig } from "@/components/shared/container/site";
 import { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
+const getBaseUrl = () => {
+  if (process.env.NODE_ENV === "production") {
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  }
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+};
 
 export const metadata: Metadata = {
-  title: siteConfig.name,
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
   description: siteConfig.description,
   // Add the following metadata:
 
@@ -16,22 +25,23 @@ export const metadata: Metadata = {
   openGraph: {
     title: siteConfig.name,
     description: siteConfig.description,
-    type: "website",
-    locale: "en_US",
   },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  metadataBase: new URL(getBaseUrl()),
   twitter: {
     card: "summary",
     site: "@tigawanna", // Replace with your Twitter handle
     creator: "@tigawanna", // Replace with your Twitter handle
   },
-
 };
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className+"bg-red-400"}>
+
+      <body className={inter.className}>
         <PageContainer>{children}</PageContainer>
       </body>
     </html>
