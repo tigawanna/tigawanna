f61b94212a7cde1d4305c7639d9ecfd4b8409582
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-01-13 18:31:36 +0300

    fix possibly null links

--- src/app/project/components/OneRepo.tsx
+++ src/app/project/components/OneRepo.tsx
@@ -1,36 +1,55 @@
 import { Suspense } from "react";
 import { OneGithubRepoLanguages } from "./OneGithubRepoLanguages";
 import { getOneRepoGQL } from "../helpers/getOneRepoGQL";
-import {  ChevronLeft, GithubIcon, Globe, Loader } from "lucide-react";
+import { ChevronLeft, GithubIcon, Globe, Loader } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { StackblitzEmbed } from "./stackblitzEmbed";
 
-
-
-export async function OneRepo({ params }: { params: string }) {
+export async function OneRepo({ params, ...props }: { params: string }) {
   const [owner, repo] = params.split("%3D%3E");
-  const query = await getOneRepoGQL({ owner, repo });
-  const data = query
-  
-  if(!data || !data.data){
-  return (
-    <div className="w-full h-screen flex flex-col  items-center gap-5 relative bg-base-300">
-      <Image
-        src="/blobby.svg"
-        alt="blobby background"
-        priority
-        height={1000}
-        width={1000}
-        className="h-screen w-full object-fit  absolute top-0 bottom-0"
-      />
 
-      <div className="w-full h-full flex text-4xl font-bold z-20  gap-2 justify-center items-center">
-        Repository {owner}/{repo} not found
+  if (!owner || !repo) {
+    return (
+      <div className="w-full h-screen flex flex-col  items-center gap-5 relative bg-base-300">
+        <Image
+          src="/blobby.svg"
+          alt="blobby background"
+          priority
+          height={1000}
+          width={1000}
+          className="h-screen w-full object-fit  absolute top-0 bottom-0"
+        />
+
+        <div className="w-full h-full flex text-4xl font-bold z-20  gap-2 justify-center items-center">
+          Repository {owner}/{repo} not found
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  const query = await getOneRepoGQL({ owner, repo });
+  const data = query;
+  console.log("==== data === ", data);
+  if (!data || !data.data) {
+    return (
+      <div className="w-full h-screen flex flex-col  items-center gap-5 relative bg-base-300">
+        <Image
+          src="/blobby.svg"
+          alt="blobby background"
+          priority
+          height={1000}
+          width={1000}
+          className="h-screen w-full object-fit  absolute top-0 bottom-0"
+        />
+
+        <div className="w-full h-full flex text-4xl font-bold z-20  gap-2 justify-center items-center">
+          Repository {owner}/{repo} not found
+        </div>
+      </div>
+    );
+  }
+  const homepage_url = data?.data?.repository?.homepageUrl;
+  const repo_url = data?.data?.repository?.url;
   return (
     <div className="w-full h-full min-h-screen flex flex-col  items-center gap-5 pb-5">
       <div className="w-full h-full flex sm:max-h-[40%]   gap-2 justify-center items-center p-5">
@@ -41,20 +60,24 @@ export async function OneRepo({ params }: { params: string }) {
             </Link>
             <h1 className="text-3xl font-bold">{data?.data?.repository?.name}</h1>
             <div className="flex gap-7 items-center bg-base-100 p-4 rounded-xl">
-              <Link
-                href={data?.data?.repository?.homepageUrl}
-                target="_blank"
-                className="flex items-center justify-center gap-1 text-sm hover:text-secondary">
-                <Globe className="w-5 h-5" />
-                site
-              </Link>
-              <Link
-                href={data?.data?.repository?.url}
-                className="text-sm hover:text-secondary flex items-center gap-2 "
-                target="_blank"
-                rel="noreferrer">
-                View on Github <GithubIcon className="w-5 h-5" />
-              </Link>
+              {homepage_url && (
+                <Link
+                  href={homepage_url}
+                  target="_blank"
+                  className="flex items-center justify-center gap-1 text-sm hover:text-secondary">
+                  <Globe className="w-5 h-5" />
+                  site
+                </Link>
+              )}
+              {repo_url && (
+                <Link
+                  href={repo_url}
+                  className="text-sm hover:text-secondary flex items-center gap-2 "
+                  target="_blank"
+                  rel="noreferrer">
+                  View on Github <GithubIcon className="w-5 h-5" />
+                </Link>
+              )}
             </div>
           </div>
 
@@ -96,8 +119,7 @@ export async function OneRepo({ params }: { params: string }) {
   );
 }
 
-
-export function OneRepoSuspenseFallback(){
+export function OneRepoSuspenseFallback() {
   return (
     <div className="w-full h-screen flex flex-col  items-center gap-5 relative bg-base-100">
       <div className="h-44 w-full bg-base-200 skeleton"></div>
@@ -105,9 +127,8 @@ export function OneRepoSuspenseFallback(){
       <span className="text-3xl font-bold h-6 rounded-xl bg-base-200 skeleton"></span>
       <span className="text-3xl font-bold h-6 rounded-xl bg-base-200 skeleton"></span>
       <div className="h-full w-full flex justify-center items-center">
-        <Loader className="animate-spin"/>
+        <Loader className="animate-spin" />
       </div>
-
     </div>
   );
 }
