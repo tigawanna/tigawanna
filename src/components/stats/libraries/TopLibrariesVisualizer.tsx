411b5491aadae2d30df237d2d8f4701e2e4cf461
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-03 06:36:48 +0300

    add packges disclaimer

--- src/components/stats/libraries/TopLibrariesVisualizer.tsx
+++ src/components/stats/libraries/TopLibrariesVisualizer.tsx
@@ -145,6 +145,20 @@ export function TopLibrariesVisualizer({ libs }: TopLibrariesVisualizerProps) {
           }}
         </ParentSize>
       </div>
+      <div role="alert" className="alert bg-opacity-30 rounded-xl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="stroke-info shrink-0 w-6 h-6">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+        </svg>
+        <span>Images above might be inaccurate</span>
+      </div>
     </div>
   );
 }
