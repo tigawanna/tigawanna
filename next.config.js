181c69bdb2ede9b9c62e09b2125c105b1aab6cd3
tigawanna <denniskinuthiawaweru@gmail.com>
2024-10-25 18:28:26 +0300

    fix devto images not showing they moved to :media2.dev.to

--- next.config.js
+++ next.config.js
@@ -1,46 +1,50 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	//   experimental: {
-	//     serverActions: true,
-	//   },
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "127.0.0.1",
-			},
-			{
-				protocol: "https",
-				hostname: "tigawanna-pocketbase.fly.dev",
-			},
-			{
-				protocol: "https",
-				hostname: "opengraph.githubassets.com",
-			},
-			{
-				protocol: "https",
-				hostname: "lh3.googleusercontent.com",
-			},
-			{
-				protocol: "https",
-				hostname: "avatars.githubusercontent.com",
-			},
+  //   experimental: {
+  //     serverActions: true,
+  //   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "https",
+        hostname: "tigawanna-pocketbase.fly.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "opengraph.githubassets.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
 
-			{
-				protocol: "https",
-				hostname: "media.dev.to",
-			},
+      {
+        protocol: "https",
+        hostname: "media.dev.to",
+      },
+      {
+        protocol: "https",
+        hostname: "media2.dev.to",
+      },
 
-			{
-				protocol: "https",
-				hostname: "repository-images.githubusercontent.com",
-			},
-		],
-	},
+      {
+        protocol: "https",
+        hostname: "repository-images.githubusercontent.com",
+      },
+    ],
+  },
 
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
